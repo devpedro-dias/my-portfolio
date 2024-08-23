49dff1cb0d47de338554388cb5a64a6d47acf8fd
Pedro Dias <pedrocorchogdev@gmail.com>
2024-08-23 20:00:07 -0300

    fix: navbar issue

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import Tools from "@/components/Tools";
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-crop mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
